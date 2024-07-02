957c525777a7fb2a1b0adef41ab5bf4af1c418ca
brandendupont-mcw <bdupont@mcw.edu>
2024-07-02 19:05:55 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,16 +79,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-95 dark:text-white">
+      <body className="">
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
         
           <SectionContainer>
             <div className="flex h-screen flex-col justify-between font-sans">
             <Header2/>
-     
-            <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
-                <main className="mb-auto">{children}</main>
-                </SearchProvider>
+            <main className="">{children}</main>
+ 
             </div>
           </SectionContainer>
       </body>

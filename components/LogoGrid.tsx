fb224667f49e7e110e9ec38ee63fc371b209c7e9
brandendupont-mcw <bdupont@mcw.edu>
2024-07-03 23:20:54 -0400

    Update LogoGrid.tsx

--- components/LogoGrid.tsx
+++ components/LogoGrid.tsx
@@ -6,7 +6,7 @@ function LogoGrid() {
         <section className=" ">
         <div className="px-8 py-24  md:px-12 lg:px-32 mx-auto ">
           
-          <div className="grid grid-cols 2xl:gap-0.5 xl:gap-10 2xl:grid-cols-5  justify-center">
+          <div className="grid grid-cols 2xl:gap-0.5 gap-10 xl:gap-10 2xl:grid-cols-5  justify-center">
             <div className="text-4xl">Project Partners</div>
           <div className="flex justify-center col-span-1 px-8">
               <img className="max-h-16" src="../static/images/cdac-logo.png" alt="logo"/>

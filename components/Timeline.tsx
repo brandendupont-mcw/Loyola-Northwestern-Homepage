90ef48c6c5da3b38d0d3687aa9670f8abf53c6e9
brandendupont-mcw <bdupont@mcw.edu>
2024-07-11 13:52:52 -0400

    timeline fix

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -8,77 +8,77 @@ function Timeline(){
 <h1 className="text-4xl font-semibold tracking-tight text-primary-dark sm:text-4xl text-ywhite  ">
                 Key Dates in the Colorado Prosecutorial Dashboards project 
                 </h1>
-<ol className=" mx-auto max-w-2xl relative border-s border-ywhite dark:border-gray-700 mt-16 mb-16 ">                  
+<ol className=" mx-auto max-w-2xl relative border-s border-ywhite bg-black dark:border-gray-700 mt-16 mb-16 ">                  
     <li className="mb-10 ms-4">
-        <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
+        <div className="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border  border-ywhite "></div>
         <time className="mb-1 text-sm font-normal  leading-none text-lblue  dark:text-gray-500">July, 2024</time>
         <h3 className="text-lg font-semibold text-ywhite dark:text-white">4 Dashboards Released</h3>
         <p className="mb-4 text-base font-normal text-ywhite dark:text-gray-400">The 9th, 11th, 19th, and 22nd Judicial District publicly release their data dashboards.</p>
       
     </li>
     <li className="mb-10 ms-4">
-        <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
+        <div className="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-lblue dark:text-gray-500">April, 2024</time>
         <h3 className="text-lg font-semibold text-ywhite dark:text-white">Capacity Building</h3>
         <p className="mb-4 text-base font-normal text-ywhite dark:text-gray-400">The project implements a variety of strategies to build the capacity of Judicial Districts to review, interpret, and act on data, including hosting webinars, updating the toolkit, and facilitating cross-site learning meetings. Read more <a className="text-lblue hover:underline" href="https://us10.campaign-archive.com/?u=6573e6112429578e5f88e1767&id=fd058fcd26"> here.</a>  </p>
       
     </li>
     <li className="mb-10 ms-4">
-        <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
+        <div className="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-lblue dark:text-gray-500">January, 2024</time>
         <h3 className="text-lg font-semibold text-ywhite">5 Jurisdictions Join</h3>
         <p className="mb-4 text-base font-normal text-ywhite dark:text-gray-400">Five new judicial districts join the project: the 9th, 11th, 13th, 19th, and 22nd. Read more <a className="text-lblue hover:underline" href="https://mailchi.mp/af660373c8d5/prosecutorial-dashboards-newsletter"> here.</a> </p>
       
     </li>
     <li className="mb-10 ms-4">
-        <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
+        <div className="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-lblue dark:text-gray-500">October, 2023</time>
         <h3 className="text-lg font-semibold text-ywhite dark:text-white">The Evaluation Lab</h3>
         <p className="text-base font-normal text-ywhite dark:text-gray-400">The local research partner transitions from the Colorado Evaluation and Action Lab at DU to The Evaluation Center at CU Denver. </p>
     </li>
     <li className="mb-10 ms-4">
-        <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
+        <div className="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal  leading-none text-lblue  dark:text-gray-500">July, 2023</time>
         <h3 className="text-lg font-semibold text-ywhite dark:text-white">3 Dashboards Released; 2 Years In</h3>
         <p className="mb-4 text-base font-normal text-ywhite dark:text-gray-400">The 12th, 17th, and 21st Judicial Districts publicly release their data dashboards. Read more about the work of the <a className="text-lblue hover:underline" href="https://alamosanews.com/article/da-kelly-promised-transparency-less-than-a-year-later-shes-delivering">12th</a>, <a className="text-lblue hover:underline" href="https://www.dailycamera.com/2023/07/21/17th-judicial-district-joins-data-dashboard-program/"> 17th</a>, and <a className="text-lblue hover:underline" href="https://www.gjsentinel.com/news/western_colorado/analyzing-statistics-das-office-joins-statewide-data-publishing-program/article_f3ab3198-27e8-11ee-a207-53119dab0080.html">21st.</a> The work of the first two years wraps up. Read more about the project’s influence along with lessons learned in the <a className="text-lblue hover:underline" href="https://www.the-evaluation-center.org/wp-content/uploads/2023/11/Colorado-Prosecutorial-Dashboards_Project-Survey-Results_2023.pdf">findings report.</a> </p>
       
     </li>
     <li className="mb-10 ms-4">
-        <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
+        <div className="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-lblue dark:text-gray-500">February, 2023</time>
         <h3 className="text-lg font-semibold text-ywhite dark:text-white">8 Reports on Racial and Ethnic Disparities </h3>
         <p className="mb-4 text-base font-normal text-ywhite dark:text-gray-400">Eight Judicial Districts release reports on racial and ethnic disparities at points of prosecutorial discretion. Read more in this story from <a className="text-lblue hover:underline" href="https://www.cpr.org/2023/04/12/latino-defendants-guilty-pleas/">CPR.</a>  </p>
       
     </li>
     <li className="mb-10 ms-4">
-        <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
+        <div className="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-lblue dark:text-gray-500">January, 2023 </time>
         <h3 className="text-lg font-semibold text-ywhite">5 Jurisdictions Join</h3>
         <p className="mb-4 text-base font-normal text-ywhite dark:text-gray-400">Five new Judicial Districts join the project: the 3rd, 10th, 12th, 17th, and 21st. Read more <a className="text-lblue hover:underline" href="https://coloradolab.org/wp-content/uploads/2024/06/Prosecutorial-Dashboards-Newsletter-Issue-6-2023-02.pdf/">here.</a></p>
         
     </li>
     <li className="mb-10 ms-4">
-        <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
+        <div className="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-lblue dark:text-gray-500">September, 2022</time>
         <h3 className="text-lg font-semibold text-ywhite dark:text-white">7 Dashboards Released</h3>
         <p className="text-base font-normal text-ywhite dark:text-gray-400">The 1st, 2nd, 6th, 7th, 8th, 18th, and 20th Judicial Districts publicly release their data dashboards. Read more in this story from <a className="text-lblue hover:underline" href="https://www.cpr.org/2022/09/08/colorado-district-attorney-prosecutions-data/?utm_medium=email&utm_source=lookout&utm_campaign=lookout20220909">CPR</a> and this story from the <a className="text-lblue hover:underline" href="https://nationalpost.com/pmn/news-pmn/colorado-das-unveil-data-dashboards-on-prosecutions">Associated Press.</a> </p>
     </li>
     <li className="mb-10 ms-4">
-        <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
+        <div className="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal  leading-none text-lblue  dark:text-gray-500">May, 2022</time>
         <h3 className="text-lg font-semibold text-ywhite dark:text-white">Community Listening Session</h3>
         <p className="mb-4 text-base font-normal text-ywhite dark:text-gray-400">The 1st, 2nd, 18th, and 20th Judicial Districts host a Denver metro area community listening session to understand the public’s thoughts and concerns around the role of prosecutors in Colorado. Read more<a className="text-lblue hover:underline" href="https://coloradolab.org/wp-content/uploads/2024/06/Prosecutorial-Dashboards-Newsletter-Issue-3-2022-05.pdf"> here.</a> </p>
       
     </li>
     <li className="mb-10 ms-4">
-        <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
+        <div className="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-lblue dark:text-gray-500">February, 2022</time>
         <h3 className="text-lg font-semibold text-ywhite dark:text-white">Capacity Building</h3>
         <p className="mb-4 text-base font-normal text-ywhite dark:text-gray-400">The project completes foundational steps to assess the baseline capacity of Judicial Districts, build office-based workgroups, and identify priority data points. Read more <a className="text-lblue hover:underline" href="https://coloradolab.org/wp-content/uploads/2024/06/Prosecutorial-Dashboards-Newsletter-Issue-2-2022-02.pdf"> here.</a>  </p>
       
     </li>
     <li className=" ms-4">
-        <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
+        <div className="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border  border-ywhite dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-lblue dark:text-gray-500">September, 2021</time>
         <h3 className="text-lg font-semibold text-ywhite">Funding from the Microsoft Justice Reform Initiative</h3>
         <p className="mb-4 text-base font-normal text-ywhite dark:text-gray-400">The Microsoft Justice Reform Initiative awards funding to PPI and the Colorado Evaluation and Action Lab at the DU for the project. </p>

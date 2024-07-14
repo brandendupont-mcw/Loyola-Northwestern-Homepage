8aca910a63fb36db0bbf308725fb3a2caac8ffc1
brandendupont-mcw <bdupont@mcw.edu>
2024-07-14 15:11:11 -0400

    Update carousel.tsx

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -206,7 +206,7 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "sm:absolute sm:mt-0 mt-6 sm:h-8 sm:w-8 sm:mr-0 mr-24  rounded-full",
+        "absolute sm:mt-0 mt-6 sm:h-8 sm:w-8  rounded-full",
         orientation === "horizontal"
           ? "-left-12 top-1/2 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -239,7 +239,7 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "sm:absolute sm:mt-0 mt-6 sm:h-8 sm:w-8  rounded-full",
+        "absolute sm:mt-0 mt-6 sm:h-8 sm:w-8  rounded-full",
         orientation === "horizontal"
           ? "-right-12 top-1/2 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",

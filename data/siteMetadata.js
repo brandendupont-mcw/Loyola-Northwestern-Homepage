1fd13a58c89f9bdcf351ebcb687e9e114581c075
brandendupont-mcw <bdupont@mcw.edu>
2024-07-16 14:57:13 -0400

    add media share

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/colorado-logo-branded.svg',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/media-share.svg',
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
   github: 'https://github.com',

45a73994ed32114897a13703f323058639ab8897
dstemenluc <59964325+dstemenluc@users.noreply.github.com>
2025-07-23 09:52:08 -0500

    revisions to navication

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
   { href: '/projects', title: 'Research' },
   { href: '/author', title: 'Researchers' },
@@ -17,6 +18,11 @@ const headerNavLinks = [
   { href: '/projects', title: 'Research' },
   { href: '/author', title: 'Researchers' },
   { href: '/blog', title: 'Data Stories' },
+  { href: '/author', title: 'Researchers' },
+  { href: '/blog', title: 'Data Stories' },
+>>>>>>> Stashed changes
+=======
+  { href: '/projects', title: 'Research' },
   { href: '/author', title: 'Researchers' },
   { href: '/blog', title: 'Data Stories' },
 >>>>>>> Stashed changes

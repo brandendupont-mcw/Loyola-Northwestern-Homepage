6e7c68e5c4f1aa18b7b7ae9754761f4872cfd8f9
dstemenluc <59964325+dstemenluc@users.noreply.github.com>
2025-07-24 07:17:30 -0500

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,7 +4,7 @@ import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
 
 const POSTS_PER_PAGE = 5
-
+ 
 export const metadata = genPageMetadata({ title: 'Blog' })
 
 export default function BlogPage() {
